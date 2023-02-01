2d49458b14cab92089d19688d550e2fcf3cffd7e
Hector Arroyo <harroyo1244@gmail.com>
2023-02-01 03:37:04 -0600

    added static files to static root

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -123,6 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
