a9b0cb1962ad16f8f9fefb3c53163dde516de722
Hector Arroyo <harroyo1244@gmail.com>
2023-02-01 02:51:19 -0600

    fixed static root

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
